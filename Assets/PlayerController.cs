665f97ce1bdb7adb17b70d84a06ce5d9709f9015
Сергей Белоусов <sergey.belousov@playrix.com>
2023-04-01 12:52:51 +0200

    Игрок появляется в середине камеры

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,23 +5,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject camera;
     public float speed = 5f; // скорость движения точки
 
     // вызывается при запуске игры
-    private void Start()
-    {
+    private void Start() {
+        var cameraComp = camera.GetComponent<Camera>();
+        float cameraHeight = 2f * cameraComp.orthographicSize;
+        float cameraWidth = cameraHeight * cameraComp.aspect;
+        float cameraLeft = cameraComp.transform.position.x - cameraWidth / 2f;
+        float cameraRight = cameraComp.transform.position.x + cameraWidth / 2f;
+        float cameraBottom = cameraComp.transform.position.y - cameraHeight / 2f;
+        float cameraTop = cameraComp.transform.position.y + cameraHeight / 2f;
+        var cameraCenter = camera.GetComponent<Transform>().position;
+        cameraCenter.z = 0;
+        transform.Translate(cameraCenter);
     }
 
-    private void Update()
-    {
+    private void Update() {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         var direction = new Vector2(horizontal, vertical);
 
         transform.Translate(direction * Time.deltaTime * speed);
     }
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-      
+    private void OnTriggerEnter2D(Collider2D other) {
+
     }
 }
