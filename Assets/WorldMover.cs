dd385777bedb5f5082c0e66a5394025abc69a03d
Pavel Zubarev <pavel.zubarev@playrix.com>
2023-04-02 16:18:52 +0200

    Добавил ускорение игры

--- Assets/WorldMover.cs
+++ Assets/WorldMover.cs
@@ -6,17 +6,29 @@ public class WorldMover : MonoBehaviour
 {
     public float speed = 1f;
     private bool enable = true;
+    public PlayerController playerController;
+    public int coinsThreshold = 10;
+    public float speedIncreece = 0.1f;
+    public float maxSpeed = 2.5f;
+    private int currentThreshold = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        currentThreshold = coinsThreshold;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (enable) {
-            transform.Translate(new Vector2(0, 1) * Time.deltaTime * speed);
+            transform.Translate(new Vector2(0, 1) * Time.deltaTime * -speed);
+
+            if (playerController.score > currentThreshold) {
+                currentThreshold += coinsThreshold;
+                speed += speedIncreece;
+                speed = Mathf.Max(speed, maxSpeed);
+            }
         }
     }
 
